0de09d72090e3f198647f04d21577625e17eb05d
lucaa.mendes <lucas1999mendes@gmail.com>
2020-11-17 21:52:56 -0300

    Inicio Fase 2 - Alterado os modificadores das variaveis de privado para publico

--- app/src/main/java/com/projFg/moreaqui/model/ImovelModel.java
+++ app/src/main/java/com/projFg/moreaqui/model/ImovelModel.java
@@ -1,10 +1,10 @@
 package com.projFg.moreaqui.model;
 
 public final class ImovelModel {
-    private final int telefoneImovel;
-    private final String tipoImovel;
-    private final String tamanhoImovel;
-    private final Boolean emConstrucaoImovel;
+    public final int telefoneImovel;
+    public final String tipoImovel;
+    public final String tamanhoImovel;
+    public final Boolean emConstrucaoImovel;
 
     public ImovelModel(int telefoneImovel,String tipoImovel,String tamanhoImovel,Boolean emConstrucaoImovel){
         this.telefoneImovel = telefoneImovel;
@@ -12,5 +12,6 @@ public final class ImovelModel {
         this.tamanhoImovel = tamanhoImovel;
         this.emConstrucaoImovel = emConstrucaoImovel;
     }
+
 
 }
